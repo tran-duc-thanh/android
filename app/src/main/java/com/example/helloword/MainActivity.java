3500f5659ccfe61838a6aed7e5d740f111f1525d
Tran Duc Thanh <tranducthanh04101996@gmail.com>
2023-02-18 21:05:06 +0700

    commit code

--- app/src/main/java/com/example/helloword/MainActivity.java
+++ app/src/main/java/com/example/helloword/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.constraint_layout_regiter);
+        setContentView(R.layout.constraint_layout_login);
     }
 }
