7997301a48f3a4838c550656a3a1b4d54747ff17
Tran Duc Thanh <tranducthanh04101996@gmail.com>
2023-02-22 11:57:28 +0700

    commit code

---
+++ app/src/main/java/com/example/helloword/ListViewActivity.java
@@ -0,0 +1,27 @@
+package com.example.helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+
+    private void init (View view) {
+        listView = findViewById(R.id.listView);
+    }
+}
