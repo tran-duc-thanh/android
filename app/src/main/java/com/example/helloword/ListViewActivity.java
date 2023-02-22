7d19c72c05af33015cc2a5119b0eb24b2625ac9c
Tran Duc Thanh <tranducthanh04101996@gmail.com>
2023-02-22 15:22:23 +0700

    commit code

--- app/src/main/java/com/example/helloword/ListViewActivity.java
+++ app/src/main/java/com/example/helloword/ListViewActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,14 +15,21 @@ import java.util.List;
 public class ListViewActivity extends AppCompatActivity {
 
     private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        init();
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Toast.makeText(getApplicationContext(), arrayAdapter.getItem(i), Toast.LENGTH_SHORT).show();
+        });
     }
 
-    private void init (View view) {
+    private void init() {
         listView = findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item_view, getResources().getStringArray(R.array.list));
+        listView.setAdapter(arrayAdapter);
     }
 }
